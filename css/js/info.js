80717c0a808d7c11a1c1f1c2982c7651b85bd0e9
kaitey168 <134349514+kaitey168@users.noreply.github.com>
2023-05-24 09:45:49 +0800

    采集js
    采集js

---
+++ css/js/info.js
@@ -0,0 +1,11 @@
+
+
+var SiteInfo={"siteName":"暴风资源",
+    "playerFlag":"bfzym3u8",
+    "siteHost": "http://bfzy.tv/",
+    "siteBackTip":"(1-10)均可使用",
+    "apiHost": "https://bfzyapi.com/",
+    "playerHost": "https://bfzyplayer.com/player/",
+    "tgUrl":"https://t.me/bfzy_tv",
+
+}
